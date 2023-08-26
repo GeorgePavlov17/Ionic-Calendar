c14626b141298ffd4d5bb95dce492a9371234fdb
Georgi Pavlov <georgipavlov17@gmail.com>
2023-08-26 12:59:49 +0300

    calendar views

--- Ionic-Calendar/src/app/home/home.page.ts
+++ Ionic-Calendar/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CalendarMode } from 'ionic6-calendar';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  calendar = {
+    mode: 'month' as CalendarMode
+  };
 
   constructor() {}
 
