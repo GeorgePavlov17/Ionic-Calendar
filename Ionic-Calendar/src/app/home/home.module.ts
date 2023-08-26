cc78a43f90f97eca7d4340d642a0738501e04029
Georgi Pavlov <georgipavlov17@gmail.com>
2023-08-26 13:27:04 +0300

    importing angular locale calendar options

--- Ionic-Calendar/src/app/home/home.module.ts
+++ Ionic-Calendar/src/app/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
@@ -6,6 +6,9 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 import { NgCalendarModule } from 'ionic6-calendar';
+import { registerLocaleData } from '@angular/common';
+import localeBg from '@angular/common/locales/bg';
+registerLocaleData(localeBg, 'bg-BG');
 
 
 @NgModule({
@@ -16,6 +19,7 @@ import { NgCalendarModule } from 'ionic6-calendar';
     HomePageRoutingModule,
     NgCalendarModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [{provide: LOCALE_ID, useValue: 'bg-BG'}]
 })
 export class HomePageModule {}
