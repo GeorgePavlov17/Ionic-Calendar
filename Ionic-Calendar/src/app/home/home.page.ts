4045c9f9a5159bf259454e60726164f47ee60a81
Georgi Pavlov <georgipavlov17@gmail.com>
2023-08-26 16:56:37 +0300

    adding ionic storage and eventSource to the calendar

--- Ionic-Calendar/src/app/home/home.page.ts
+++ Ionic-Calendar/src/app/home/home.page.ts
@@ -1,14 +1,17 @@
-import { Component, ViewChild } from '@angular/core';
-import { IonRouterOutlet } from '@ionic/angular';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { IonModal, IonRouterOutlet } from '@ionic/angular';
+import { format, parseISO, toDate } from 'date-fns';
 import { CalendarComponent, CalendarMode } from 'ionic6-calendar';
+import { CalEvent, EventsService } from '../services/events.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   @ViewChild(CalendarComponent) myCal!: CalendarComponent;
+  @ViewChild('modal') modal!: IonModal;
 
   calendar = {
     mode: 'month' as CalendarMode,
@@ -18,16 +21,28 @@ export class HomePage {
   newEvent = {
     title: '',
     allDay: false,
-    startTime: null,
-    endTime: null
+    startTime: '',
+    endTime: ''
   };
 
+  public showStart: boolean = false;
+  public showEnd: boolean = false;
+  public formattedStart = '';
+  public formattedEnd = '';
   public viewTitle = '';
   public eventSource: any[] = [];
   public presentingtElement: any = null;
 
-  constructor(private ionRouterOutlet: IonRouterOutlet) {
+  constructor(
+    private ionRouterOutlet: IonRouterOutlet, 
+    private eventsService: EventsService
+    ) 
+    {
     this.presentingtElement = ionRouterOutlet.nativeEl;
+  }
+
+  async ngOnInit() {
+    this.eventSource = await this.eventsService.getData();
   }
 
   setToday() {
@@ -43,8 +58,48 @@ export class HomePage {
     this.myCal.slideNext();
   }
 
-  scheduleEvent() {
+  onTimeSelected(ev: {selectedTime: Date; events: any[]}) {
+    this.formattedStart = format(ev.selectedTime, 'HH:mm, MMM d, yyyy');
+    this.newEvent.startTime = format(ev.selectedTime, "yyyy-MM-dd'T'HH:mm:ss");
 
+    const later = ev.selectedTime.setHours(ev.selectedTime.getHours() + 1);
+    this.formattedEnd = format(later, 'HH:mm, MMM d, yyyy');
+    this.newEvent.endTime = format(later, "yyyy-MM-dd'T'HH:mm:ss");
+
+    if(this.calendar.mode === 'day' || this.calendar.mode === 'week') {
+      this.modal.present();
+    }
   }
 
+  startChanged(value: any) {
+    this.newEvent.startTime = value;
+    this.formattedStart = format(parseISO(value), 'HH:mm, MMM d, yyyy');
+  }
+
+  endChanged(value: any) {
+    this.newEvent.endTime = value;
+    this.formattedEnd = format(parseISO(value), 'HH:mm, MMM d, yyyy');
+  }
+
+  scheduleEvent() {
+    const toAdd: CalEvent = {
+      title: this.newEvent.title,
+      startTime: new Date(this.newEvent.startTime),
+      endTime:  new Date(this.newEvent.endTime),
+      allDay: this.newEvent.allDay
+    };
+    
+    this.eventSource.push(toAdd);
+    this.myCal.loadEvents();
+    this.eventsService.addData(toAdd);
+
+    this.newEvent = {
+      title: '',
+      allDay: false,
+      startTime: '',
+      endTime: '',
+    };
+
+    this.modal.dismiss();
+  }
 }
